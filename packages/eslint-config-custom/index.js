a5941647d33fd2a5ddd6f9ad41fa24db87ab94d7
Wojtazzzz <marcin.witas72@gmail.com>
2023-09-23 18:57:38 +0200

    chore/eslint, prettier

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+	extends: [
+		"turbo",
+		"prettier",
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: "latest",
+	},
+	rules: {
+		"@typescript-eslint/no-unused-vars": "error",
+		"@typescript-eslint/no-explicit-any": "error",
+		"@typescript-eslint/consistent-type-imports": "warn",
+	},
+};
