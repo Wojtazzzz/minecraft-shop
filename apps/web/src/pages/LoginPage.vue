23d3fcc50b8cfa3f56efc2cccea8c1cb0f15d9bc
Wojtazzzz <marcin.witas72@gmail.com>
2023-10-22 12:47:11 +0200

    refactor/add global axios instance

--- apps/web/src/pages/LoginPage.vue
+++ apps/web/src/pages/LoginPage.vue
@@ -4,7 +4,7 @@ import Input from '@/components/inc/Input.vue';
 import { useForm, ErrorMessage } from 'vee-validate';
 import * as yup from 'yup';
 import Container from '@/components/inc/Container.vue';
-import axios from 'axios';
+import { axios } from '@/utils/axios';
 
 const validationSchema = yup.object({
     login: yup.string().required(),
@@ -19,72 +19,74 @@ const login = defineComponentBinds('login');
 const password = defineComponentBinds('password');
 
 const submit = handleSubmit(async (values) => {
-    await axios.post(
-        'http://localhost:8000/auth/login',
-        {
-            login: values.login,
-            password: values.password,
-        },
-        {
-            withCredentials: true,
-        },
-    );
-
-    await axios.get('http://localhost:8000/auth/me', {
-        withCredentials: true,
+    await axios.post('/auth/login', {
+        login: values.login,
+        password: values.password,
     });
+
+    await axios.get('/auth/me');
 });
 </script>
 
 <template>
     <div class="w-screen h-screen flex flex-col justify-center items-center">
         <Container>
-            <section class="max-w-md w-full rounded-md shadow-md border-[1px] border-gray-100 p-5">
-                <div class="w-full">
-                    <div class="w-full border-b-[1px] border-gray-100 pb-5">
-                        <h2 class="text-xl font-medium text-gray-600">Login</h2>
+            <div class="flex justify-center">
+                <section
+                    class="max-w-md w-full rounded-md shadow-md border-[1px] border-gray-100 p-5"
+                >
+                    <div class="w-full">
+                        <div class="w-full border-b-[1px] border-gray-100 pb-5">
+                            <h2 class="text-xl font-medium text-gray-600">Login</h2>
+                        </div>
+
+                        <form
+                            novalidate
+                            @submit.prevent="submit"
+                            class="flex flex-col gap-y-4 mt-5"
+                        >
+                            <div>
+                                <label class="space-y-1">
+                                    <small class="text-gray-600 font-medium">NICKNAME</small>
+                                    <Input
+                                        type="text"
+                                        name="login"
+                                        v-bind="login"
+                                        placeholder="CrazyJohny123"
+                                        autocomplete="username"
+                                    />
+                                    <ErrorMessage name="login" />
+                                </label>
+                            </div>
+
+                            <div>
+                                <label class="space-y-1">
+                                    <small class="text-gray-600 font-medium">PASSWORD</small>
+                                    <Input
+                                        type="password"
+                                        name="password"
+                                        v-bind="password"
+                                        placeholder="********"
+                                        autocomplete="current-password"
+                                    />
+                                    <ErrorMessage name="password" />
+                                </label>
+                            </div>
+
+                            <div class="mt-6">
+                                <Button type="submit">LOGIN</Button>
+                            </div>
+                        </form>
                     </div>
 
-                    <form novalidate @submit.prevent="submit" class="flex flex-col gap-y-4 mt-5">
-                        <div>
-                            <label class="space-y-1">
-                                <small class="text-gray-600 font-medium">NICKNAME</small>
-                                <Input
-                                    type="text"
-                                    name="login"
-                                    v-bind="login"
-                                    placeholder="CrazyJohny123"
-                                    autocomplete="username"
-                                />
-                                <ErrorMessage name="login" />
-                            </label>
-                        </div>
-
-                        <div>
-                            <label class="space-y-1">
-                                <small class="text-gray-600 font-medium">PASSWORD</small>
-                                <Input
-                                    type="password"
-                                    name="password"
-                                    v-bind="password"
-                                    placeholder="********"
-                                    autocomplete="current-password"
-                                />
-                                <ErrorMessage name="password" />
-                            </label>
-                        </div>
-
-                        <div class="mt-6">
-                            <Button type="submit">LOGIN</Button>
-                        </div>
-                    </form>
-                </div>
-
-                <div class="mt-8 text-gray-600">
-                    <span>Don't have an account? </span>
-                    <RouterLink :to="{ name: 'login' }" class="underline">Register now</RouterLink>
-                </div>
-            </section>
+                    <div class="mt-8 text-gray-600">
+                        <span>Don't have an account? </span>
+                        <RouterLink :to="{ name: 'login' }" class="underline"
+                            >Register now</RouterLink
+                        >
+                    </div>
+                </section>
+            </div>
         </Container>
     </div>
 </template>
