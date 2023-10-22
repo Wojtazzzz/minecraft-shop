b791489e391ac60c1495e8d0f3f95e587d98ec9f
Wojtazzzz <marcin.witas72@gmail.com>
2023-10-22 12:24:51 +0200

    ci/fix build error

--- apps/web/src/components/inc/Input.vue
+++ apps/web/src/components/inc/Input.vue
@@ -7,15 +7,20 @@ defineProps<{
 }>();
 
 const emit = defineEmits(['update:modelValue', 'blur']);
+
+const updateValue = (event: Event) => {
+    emit('update:modelValue', (event.target as HTMLInputElement).value);
+};
 </script>
 
 <template>
     <input
         :type="type"
         :name="name"
+        :value="modelValue"
         class="w-full rounded bg-[#EFF4F6] p-3 outline-none shadow"
         :placeholder="placeholder"
-        @input="$emit('update:modelValue', $event.target.value)"
+        @input="updateValue"
         @blur="$emit('blur')"
     />
 </template>
