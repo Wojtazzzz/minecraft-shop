2fe2d33f2e4a136cfd5e80f3672c9f07e6f085d5
Wojtazzzz <marcin.witas72@gmail.com>
2023-10-22 13:56:37 +0200

    refactor(LoginPage)/add vue-query to requests

--- apps/web/src/pages/LoginPage/useLogin.ts
+++ apps/web/src/pages/LoginPage/useLogin.ts
@@ -1,26 +1,36 @@
 import * as yup from 'yup';
 import { useForm } from 'vee-validate';
 import { axios } from '@/utils/axios';
+import { useMutation } from '@tanstack/vue-query';
+import router from '@/router';
 
-const validationSchema = yup.object({
+const schema = yup.object({
     login: yup.string().required(),
     password: yup.string().required(),
 });
 
+type LoginPayload = yup.InferType<typeof schema>;
+
+const mutationFn = async (payload: LoginPayload) => {
+    await axios.post('/auth/login', payload);
+};
+
 export const useLogin = () => {
-    const { defineComponentBinds, handleSubmit } = useForm({
-        validationSchema,
+    const { defineComponentBinds, handleSubmit } = useForm<LoginPayload>({
+        validationSchema: schema,
+    });
+
+    const { mutate } = useMutation({
+        mutationFn,
+        onSuccess: async () => {
+            await router.push({ name: 'home' });
+        },
     });
 
     const login = defineComponentBinds('login');
     const password = defineComponentBinds('password');
 
-    const submit = handleSubmit(async (values) => {
-        await axios.post('/auth/login', {
-            login: values.login,
-            password: values.password,
-        });
-    });
+    const submit = handleSubmit((values) => mutate(values));
 
     return {
         login,
