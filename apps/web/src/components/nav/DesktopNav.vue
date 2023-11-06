c52a0f646ae0a79c801b5219e4fe136dcab923c7
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-06 22:36:59 +0100

    feat/nav component

---
+++ apps/web/src/components/nav/DesktopNav.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { links, buttons } from './navLinks';
+import ButtonAsLink from '@/components/inc/ButtonAsLink.vue';
+</script>
+
+<template>
+    <ul role="list" class="items-center hidden space-x-8 lg:flex">
+        <li v-for="({ href, name }, index) in links" :key="index">
+            <RouterLink :to="href" class="font-medium tracking-wide text-gray-800">
+                {{ name }}
+            </RouterLink>
+        </li>
+
+        <li v-for="({ href, name }, index) in buttons" :key="index">
+            <ButtonAsLink :href="href">{{ name }}</ButtonAsLink>
+        </li>
+    </ul>
+</template>
---
+++ apps/web/src/components/nav/navLinks.ts
@@ -0,0 +1,17 @@
+export const links = [
+    {
+        name: 'Home',
+        href: '/',
+    },
+    {
+        name: 'Shop',
+        href: '/shop',
+    },
+];
+
+export const buttons = [
+    {
+        name: 'Login',
+        href: '/login',
+    },
+];
