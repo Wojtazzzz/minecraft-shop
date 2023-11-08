59dd9b4a7a9daa1b66fc4ead331c994977ab3c1d
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-08 20:35:35 +0100

    feat/add helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+// import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.use(helmet());
 
 	app.enableCors({
 		origin: 'http://localhost:3000',
