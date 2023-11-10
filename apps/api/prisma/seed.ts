37a269149ca8243b3cc82395d473f7ab1a33b678
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-10 21:56:50 +0100

    fix(product seed)/change image url

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -24,7 +24,7 @@ async function main() {
 			description: 'Vip 30 provides many exciting benefits!',
 			price: 10.0,
 			active: true,
-			image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fxgp.pl%2Fporadniki%2Fjak-zrobic-ksiazke-w-minecraft%2F&psig=AOvVaw0y6iH1AOCebg1mnw-O7iB1&ust=1699730469402000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJjT1MiTuoIDFQAAAAAdAAAAABAG',
+			image: 'https://xgp.pl/wp-content/uploads/2022/07/ksiazka-minecraft-1024x576.jpg.webp',
 			commands: [
 				'add vip %s',
 				'say Hello, %s bought Vip 30d! Thank You!',
