f44247548fe153ccc9ab794f6e0e9769475266dc
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-10 20:25:39 +0100

    feat/product seed

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -4,6 +4,7 @@ import { createHash } from '../src/helpers/functions';
 const prisma = new PrismaClient();
 
 async function main() {
+	/* Users */
 	await prisma.user.deleteMany({});
 
 	await prisma.user.create({
@@ -11,6 +12,23 @@ async function main() {
 			email: 'marcin.witas@gmail.com',
 			login: 'admin',
 			password: await createHash('admin'),
+		},
+	});
+
+	/* Products */
+	await prisma.product.deleteMany({});
+
+	await prisma.product.create({
+		data: {
+			name: 'VIP 30d',
+			description: 'Vip 30 provides many exciting benefits!',
+			price: 10.0,
+			active: true,
+			image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fxgp.pl%2Fporadniki%2Fjak-zrobic-ksiazke-w-minecraft%2F&psig=AOvVaw0y6iH1AOCebg1mnw-O7iB1&ust=1699730469402000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJjT1MiTuoIDFQAAAAAdAAAAABAG',
+			commands: [
+				'add vip %s',
+				'say Hello, %s bought Vip 30d! Thank You!',
+			],
 		},
 	});
 }
