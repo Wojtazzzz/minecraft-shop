c720a10719f32c81d2d16e31e4344349e7da73fe
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-10 20:10:41 +0100

    feat/add products schema to prisma

---
+++ apps/api/prisma/migrations/20231110190457_create_product_module/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "commands" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
