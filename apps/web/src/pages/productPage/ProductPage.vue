56da8ce7697894569a4cec9a3a47582cd3562808
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-10 22:46:49 +0100

    feat(product details)/add very basic product details page

---
+++ apps/web/src/pages/productPage/ProductPage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useGetProduct } from './useGetProduct';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const { isSuccess, isPending, isError, product } = useGetProduct(route.params.id);
+</script>
+
+<template>
+    <MainLayout>
+        <section>
+            <span v-if="isPending">Loading...</span>
+            <span v-else-if="isError || !product">Error!</span>
+
+            <article v-else-if="isSuccess">
+                <h2>Product {{ product.name }}</h2>
+                <h3>{{ product.description }}</h3>
+
+                <div>
+                    <img :src="product.image" alt="" width="150" height="150" />
+                </div>
+
+                <div>Price: {{ product.price.toFixed(2) }}</div>
+            </article>
+        </section>
+    </MainLayout>
+</template>
--- apps/web/src/utils/queryKeys.ts
+++ apps/web/src/utils/queryKeys.ts
@@ -1,3 +1,7 @@
 export function getProductsListQueryKey() {
     return ['products'];
 }
+
+export function getProductDetailsQueryKey(productId: number) {
+    return ['product', productId];
+}
