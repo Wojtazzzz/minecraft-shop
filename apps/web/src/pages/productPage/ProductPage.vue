e9c37475d4329a769084b4444f526226f21afaa1
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-13 17:56:28 +0100

    refactor(ProductPage)/add better layout

--- apps/web/src/pages/productPage/ProductPage.vue
+++ apps/web/src/pages/productPage/ProductPage.vue
@@ -2,6 +2,11 @@
 import { useGetProduct } from './useGetProduct';
 import { useRoute } from 'vue-router';
 import { useBuyProduct } from './useBuyProduct';
+import Container from '@/components/inc/Container.vue';
+import MainLayout from '@/layouts/MainLayout.vue';
+import Spinner from '@/components/icons/Spinner.vue';
+import ApiErrorMessage from '@/components/inc/ApiErrorMessage.vue';
+import Button from '@/components/inc/Button.vue';
 
 const route = useRoute();
 const { isSuccess, isPending, isError, product } = useGetProduct(Number(route.params.id));
@@ -10,24 +15,46 @@ const { buyProduct } = useBuyProduct(Number(route.params.id));
 
 <template>
     <MainLayout>
-        <section>
-            <span v-if="isPending">Loading...</span>
-            <span v-else-if="isError || !product">Error!</span>
-
-            <article v-else-if="isSuccess">
-                <h2>Product {{ product.name }}</h2>
-                <h3>{{ product.description }}</h3>
-
-                <div>
-                    <img :src="product.image" alt="" width="150" height="150" />
+        <Container>
+            <section aria-live="polite" :aria-busy="isPending" class="max-w-4xl mx-auto">
+                <div v-if="isPending">
+                    <Spinner size="lg" color="accent" />
                 </div>
 
-                <div>Price: {{ product.price.toFixed(2) }}</div>
+                <div v-else-if="isError || !product" role="alert">
+                    <ApiErrorMessage
+                        error="Coś poszło nie tak, proszę spróbować ponownie później"
+                    />
+                </div>
 
-                <form novalidate @submit.prevent="buyProduct" class="flex flex-col gap-y-4 mt-5">
-                    <Button type="submit">Kupuję</Button>
-                </form>
-            </article>
-        </section>
+                <article
+                    v-else-if="isSuccess"
+                    class="text-center w-full flex flex-col gap-y-8 items-center"
+                >
+                    <header class="w-full space-y-2 border-b pb-5">
+                        <h2 class="text-3xl lg:text-4xl font-medium">{{ product.name }}</h2>
+                        <p class="text-gray-70">{{ product.description }}</p>
+                    </header>
+
+                    <div>
+                        <img :src="product.image" alt="" class="w-56 h-56 lg:w-80 lg:h-80" />
+                    </div>
+
+                    <div>Price: {{ product.price.toFixed(2) }}</div>
+
+                    <p class="text-gray-700 text-sm">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos iure nesciunt
+                        nulla veniam, eos laborum voluptates enim placeat! Vel eaque recusandae esse
+                        repudiandae ad odio sunt sint quis nobis. Commodi.
+                    </p>
+
+                    <div class="w-[300px]">
+                        <Button type="button" @click="buyProduct" :isLoading="isPending"
+                            >Kupuję</Button
+                        >
+                    </div>
+                </article>
+            </section>
+        </Container>
     </MainLayout>
 </template>
