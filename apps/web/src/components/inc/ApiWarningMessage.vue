8cd8e5a38f2f9eb2189ee82ee7fd92e076dd1dee
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-13 15:45:56 +0100

    feat/ApiWarningMessage component

---
+++ apps/web/src/components/inc/ApiWarningMessage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+    message: string;
+}>();
+</script>
+
+<template>
+    <div class="w-full px-5 py-3 text-sm font-medium rounded bg-yellow-100 text-yellow-700">
+        {{ message }}
+    </div>
+</template>
