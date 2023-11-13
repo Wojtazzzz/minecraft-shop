7312075e78e86bf249369034a72779c01926f4e0
Wojtazzzz <marcin.witas72@gmail.com>
2023-11-13 19:36:38 +0100

    feat/add hero section

--- apps/web/src/layouts/MainLayout.vue
+++ apps/web/src/layouts/MainLayout.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import Nav from '../components/nav/Nav.vue';
+import Hero from '../components/hero/Hero.vue';
 </script>
 <template>
     <div>
         <Nav />
 
-        <div class="mt-32">
-            <main>
-                <slot></slot>
-            </main>
+        <div class="mt-16">
+            <Hero />
+
+            <div class="mt-12">
+                <main>
+                    <slot></slot>
+                </main>
+            </div>
         </div>
     </div>
 </template>
