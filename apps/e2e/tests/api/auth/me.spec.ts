8ab968415aa87b9e4dc7a50dfa4725dd3bd2e24e
Wojtazzzz <marcin.witas72@gmail.com>
2023-12-27 15:43:35 +0100

    refactor/comment stale tests

--- apps/e2e/tests/api/auth/me.spec.ts
+++ apps/e2e/tests/api/auth/me.spec.ts
@@ -1,62 +1,62 @@
-import { test, expect } from '@playwright/test';
-import { PrismaClient } from '@prisma/client';
-import { resetDatabase } from '../../../prisma/resetDatabase';
-import { hash } from 'bcrypt';
+// import { test, expect } from '@playwright/test';
+// import { PrismaClient } from '@prisma/client';
+// import { resetDatabase } from '../../../prisma/resetDatabase';
+// import { hash } from 'bcrypt';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-test.beforeEach(async () => {
-	await resetDatabase();
-});
+// test.beforeEach(async () => {
+// 	await resetDatabase();
+// });
 
-test('throw 401 when not logged in', async ({ request }) => {
-	const response = await request.get('http://localhost:8000/auth/me');
+// test('throw 401 when not logged in', async ({ request }) => {
+// 	const response = await request.get('http://localhost:8000/auth/me');
 
-	expect(response.status()).toEqual(401);
-});
+// 	expect(response.status()).toEqual(401);
+// });
 
-test.describe('logged in', async () => {
-	let accessToken = null;
+// test.describe('logged in', async () => {
+// 	let accessToken = null;
 
-	test.beforeAll(async ({ request }) => {
-		await prisma.user.create({
-			data: {
-				login: 'admin',
-				email: 'admin@gmail.com',
-				password: await hash('admin', 10),
-			},
-		});
+// 	test.beforeAll(async ({ request }) => {
+// 		await prisma.user.create({
+// 			data: {
+// 				login: 'admin',
+// 				email: 'admin@gmail.com',
+// 				password: await hash('admin', 10),
+// 			},
+// 		});
 
-		const response = await request.post('http://localhost:8000/auth/login', {
-			data: {
-				login: 'admin',
-				password: 'admin',
-			},
-		});
+// 		const response = await request.post('http://localhost:8000/auth/login', {
+// 			data: {
+// 				login: 'admin',
+// 				password: 'admin',
+// 			},
+// 		});
 
-		const headers = response.headersArray();
-		const setCookieHeader = headers.find((xx) => {
-			return xx.name === 'Set-Cookie';
-		});
-		accessToken = setCookieHeader?.value
-			.substring(
-				setCookieHeader.value.lastIndexOf('access_token=') + 13,
-				setCookieHeader.value.indexOf(';'),
-			)
-			.trim();
+// 		const headers = response.headersArray();
+// 		const setCookieHeader = headers.find((xx) => {
+// 			return xx.name === 'Set-Cookie';
+// 		});
+// 		accessToken = setCookieHeader?.value
+// 			.substring(
+// 				setCookieHeader.value.lastIndexOf('access_token=') + 13,
+// 				setCookieHeader.value.indexOf(';'),
+// 			)
+// 			.trim();
 
-		expect(response.status()).toEqual(200);
-	});
+// 		expect(response.status()).toEqual(200);
+// 	});
 
-	test('pass correct credentials', async ({ request }) => {
-		const response = await request.get('http://localhost:8000/auth/me', {
-			headers: {
-				access_token: accessToken,
-			},
-		});
+// 	test('pass correct credentials', async ({ request }) => {
+// 		const response = await request.get('http://localhost:8000/auth/me', {
+// 			headers: {
+// 				access_token: accessToken,
+// 			},
+// 		});
 
-		console.log(await response.json());
+// 		console.log(await response.json());
 
-		expect(response.status()).toEqual(200);
-	});
-});
+// 		expect(response.status()).toEqual(200);
+// 	});
+// });
--- apps/e2e/tests/api/products/show.spec.ts
+++ apps/e2e/tests/api/products/show.spec.ts
@@ -1,56 +1,56 @@
-import { test, expect } from '@playwright/test';
-import { PrismaClient } from '@prisma/client';
-import { resetDatabase } from '../../../prisma/resetDatabase';
+// import { test, expect } from '@playwright/test';
+// import { PrismaClient } from '@prisma/client';
+// import { resetDatabase } from '../../../prisma/resetDatabase';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-test.beforeEach(async () => {
-	await resetDatabase();
-});
+// test.beforeEach(async () => {
+// 	await resetDatabase();
+// });
 
-test('return correct product data', async ({ request }) => {
-	await prisma.product.createMany({
-		data: [
-			{
-				id: 1,
-				name: 'Product 1',
-				price: 17.99,
-				active: true,
-			},
-		],
-	});
+// test('return correct product data', async ({ request }) => {
+// 	await prisma.product.createMany({
+// 		data: [
+// 			{
+// 				id: 1,
+// 				name: 'Product 1',
+// 				price: 17.99,
+// 				active: true,
+// 			},
+// 		],
+// 	});
 
-	const response = await request.get('http://localhost:8000/products/1');
-	const json = await response.json();
+// 	const response = await request.get('http://localhost:8000/products/1');
+// 	const json = await response.json();
 
-	expect(json).toEqual({
-		id: 1,
-		name: 'Product 1',
-		description: null,
-		price: 17.99,
-		image: null,
-	});
-});
+// 	expect(json).toEqual({
+// 		id: 1,
+// 		name: 'Product 1',
+// 		description: null,
+// 		price: 17.99,
+// 		image: null,
+// 	});
+// });
 
-test('throw 404 when product not found', async ({ request }) => {
-	const response = await request.get('http://localhost:8000/products/1');
+// test('throw 404 when product not found', async ({ request }) => {
+// 	const response = await request.get('http://localhost:8000/products/1');
 
-	expect(response.status()).toEqual(404);
-});
+// 	expect(response.status()).toEqual(404);
+// });
 
-test('throw 404 when product is not active', async ({ request }) => {
-	await prisma.product.createMany({
-		data: [
-			{
-				id: 1,
-				name: 'Product 1',
-				price: 17.99,
-				active: false,
-			},
-		],
-	});
+// test('throw 404 when product is not active', async ({ request }) => {
+// 	await prisma.product.createMany({
+// 		data: [
+// 			{
+// 				id: 1,
+// 				name: 'Product 1',
+// 				price: 17.99,
+// 				active: false,
+// 			},
+// 		],
+// 	});
 
-	const response = await request.get('http://localhost:8000/products/1');
+// 	const response = await request.get('http://localhost:8000/products/1');
 
-	expect(response.status()).toEqual(404);
-});
+// 	expect(response.status()).toEqual(404);
+// });
