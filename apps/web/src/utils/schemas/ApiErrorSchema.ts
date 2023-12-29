cddf4c887eeb62c84acf90fc15a6de14c1ebd25b
Wojtazzzz <marcin.witas72@gmail.com>
2023-12-29 19:57:46 +0100

    refactor(app)/remove stale ApiErrorSchema

--- apps/web/src/utils/schemas/ApiErrorSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-
-export const apiErrorSchema = Yup.object({
-    response: Yup.object({
-        data: Yup.object({
-            error: Yup.string().required(),
-        }),
-    }),
-});
