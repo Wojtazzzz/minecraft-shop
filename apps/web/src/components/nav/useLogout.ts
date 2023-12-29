6a249a29df0b9384ebaf0f86be3cf3c1e2ba37bb
Wojtazzzz <marcin.witas72@gmail.com>
2023-12-29 19:46:36 +0100

    refactor(app)/add logout composable

---
+++ apps/web/src/components/nav/useLogout.ts
@@ -0,0 +1,34 @@
+import { wretch } from '@/utils/wretch';
+import { useMutation, useQueryClient } from '@tanstack/vue-query';
+import { router } from '@/router';
+
+const mutationFn = async () => {
+    return await wretch
+        .url('/auth/logout')
+        .post()
+        .unauthorized(() => {
+            throw new Error('Something went wrong, please try again later');
+        })
+        .res();
+};
+
+export const useLogout = () => {
+    const queryClient = useQueryClient();
+
+    const { mutate } = useMutation({
+        mutationFn,
+        onSuccess: async () => {
+            queryClient.invalidateQueries({ queryKey: ['user', 'auth', 'me'] });
+
+            await router.push({ name: 'products' });
+        },
+    });
+
+    function logout() {
+        mutate();
+    }
+
+    return {
+        logout,
+    };
+};
