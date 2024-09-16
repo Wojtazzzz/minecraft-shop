0dd7344873166e95800d0dbea27a3a9ea953acfb
Wojtazz <marcin.witas72@gmail.com>
2024-09-16 21:32:57 +0200

    feat(admin): add permissions to products crud

--- apps/admin/database/seeders/DatabaseSeeder.php
+++ apps/admin/database/seeders/DatabaseSeeder.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
@@ -16,23 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            PermissionSeeder::class,
+        ]);
+
         if (app()->environment() === 'production') {
             //
         }else{
+            $role = Role::findByName('admin');
+
             $user = User::factory()->create([
                 'name' => 'Wojtazzzz',
                 'email' => 'marcin.witas72@gmail.com',
             ]);
-
-            $role = Role::create([
-                'name' => 'admin',
-            ]);
-
-            $permission = Permission::create([
-                'name' => 'admin panel',
-            ]);
-
-            $role->givePermissionTo($permission);
 
             $user->assignRole($role);
 
