9611d9e4ff57a68e2def7b797397692e22e11dd6
Wojtazz <marcin.witas72@gmail.com>
2024-09-26 22:13:44 +0200

    feat(ui): add alert component

---
+++ packages/ui/src/components/alert.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { AlertCircle } from 'lucide-vue-next'
+import { Alert, AlertDescription, AlertTitle } from '../components/lib/alert'
+
+type AlertVariant = 'default' | 'destructive';
+
+const { variant = 'default' } = defineProps<{
+    variant?: AlertVariant
+}>()
+</script>
+
+<template>
+        <Alert :variant="variant">
+            <AlertCircle class="w-4 h-4" />
+            <AlertTitle>
+                <template v-if="variant==='destructive'">Error</template>
+                <template v-else>Info</template>
+            </AlertTitle>
+            <AlertDescription>
+                <slot/>
+            </AlertDescription>
+        </Alert>
+</template>
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -10,5 +10,6 @@ import CardDescription from './components/card/card-description.vue'
 import CardHeader from './components/card/card-header.vue'
 import CardTitle from './components/card/card-title.vue'
 import CardFooter from './components/card/card-footer.vue'
+import Alert from './components/alert.vue'
 
-export { Button, NavigationRoot, NavigationItem, NavigationItemLink, ToggleTheme, Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter }
+export { Button, NavigationRoot, NavigationItem, NavigationItemLink, ToggleTheme, Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter, Alert }
